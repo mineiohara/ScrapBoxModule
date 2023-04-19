a5320ed760781d1d8fb914bb156d8c53427da6a8
Yoshimine Iohara <42444881+mineiohara@users.noreply.github.com>
2023-04-19 11:56:56 +0900

    Update module.js

--- module.js
+++ module.js
@@ -1,6 +1,6 @@
 // Created by: Justin Iohara
 
-export function init(userDirectory, targetDirectory, newPageTitle, newPageBody){
+export function Initialize(){
     scrapbox.PageMenu.addMenu({
          title: 'Add',
          image: 'https://gyazo.com/9aee42065c83dfb31074fffa34faaacd/raw'
@@ -27,15 +27,13 @@ export function PasteTemplate(name, body){
         title: () => name,
         onClick: () => (function(){
             if (scrapbox.Page.lines && scrapbox.Page.lines.length == 1) {
-
-                // タイトル行をクリックしたことにする
                 const line = document.getElementById('L' + scrapbox.Page.lines[0].id)
                 const lastChar = line.querySelector('span.char-index:last-of-type')
                 const textarea = document.getElementById('text-input')
                 textarea.dispatchEvent(new KeyboardEvent('keydown', {bubbles: true, cancelable: true, keyCode: 35}))
                 lastChar.dispatchEvent(new MouseEvent('click', {bubbles: true, cancelable: true}))
 
-                   textarea.value = body
+                textarea.value = body
                 textarea.dispatchEvent(new InputEvent('input', {bubbles: true, cancelable: true}))
             }
         })()
